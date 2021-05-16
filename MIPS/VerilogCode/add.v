da9a2994a9cb9d81e56baa499668b3a319673b5b
Maedeh Shamirzaei <maedeh.shmz@gmail.com>
2021-05-16 15:55:18 +0430

    Update add.v

--- MIPS/VerilogCode/add.v
+++ MIPS/VerilogCode/add.v
@@ -1,1 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    10:37:10 07/25/2020 
+// Design Name: 
+// Module Name:    add 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module add(input [31:0] a,b,output [31:0] out);
+	
+	assign out = a + b;
+
+
+endmodule
 
