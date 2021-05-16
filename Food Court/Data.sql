f2985bbadb3006ec64b83406c343c239b0a5f243
Maedeh Shamirzaei <maedeh.shmz@gmail.com>
2021-05-16 16:12:45 +0430

    Create Data.sql

---
+++ Food Court/Data.sql
@@ -0,0 +1,172 @@
+
+INSERT INTO SALESORDERHEADER VALUES 
+(1,1,1,GETDATE(),300000,310000,'Tehran,Haghani'),
+(2,1,1,GETDATE(),100000,110000,'Tehran,Haghani'),
+(3,1,1,GETDATE(),100000,110000,'Tehran,Haghani'),
+(4,1,1,GETDATE(),100000,110000,'Tehran,Haghani'),
+(5,1,1,GETDATE(),100000,120000,'Tehran,Haghani'),
+(6,1,1,GETDATE(),100000,110000,'Tehran,Haghani'),
+(7,1,1,GETDATE(),100000,110000,'Tehran,Haghani'),
+(8,1,1,GETDATE(),100000,110000,'Tehran,Haghani'),
+(9,1,1,GETDATE(),100000,110000,'Tehran,Haghani'),
+(10,1,1,GETDATE(),100000,110000,'Tehran,Haghani'),
+(11,1,1,GETDATE(),200000,210000,'Tehran,Mirdamad'),
+(12,5,1,GETDATE(),100000,110000,'Tehran,Vanak'),
+(13,5,1,GETDATE(),100000,110000,'Tehran,Vanak')
+
+
+
+
+
+INSERT INTO FOOD_PRICES VALUES
+(1,'Dough_Small',15000),
+(2,'Water_Small',15000),
+(3,'Soda_Black_small',15000),
+(4,'Soda_Orange_small',15000),
+(5,'Soda_Lemon_small',15000),
+(6,'Yoghurt',40000),
+(7,'Salad',50000),
+(8,'Chelo_Kabab',200000),
+(9,'Chelo_Joojeh',250000),
+(10,'Chelo_Kubide_Morgh',15000),
+(11,'Vegetable_Pizza_Mini',150000),
+(12,'Chicken_Pizza_Mini',150000),
+(13,'Meat_Pizza_Mini',150000),
+(14,'Ghorme_Sabzi',150000),
+(15,'Khoresht_Ghame',150000),
+(16,'Fish_rice',230000),
+(17,'Ghelye_Fish',250000),
+(18,'Phesenjan',250000),
+(19,'Tahchin',230000)
+
+
+
+INSERT INTO BRANCHES VALUES
+(0211,'ALBORZ','MR.REZAEE',32,02188687752,'Tehran,Saharverdi_shomali,Nabshe_kooche_nikughadam'),
+(0212,'NAEB','MR.GHAFURI',32,02177610027,'Tehran,Shariati_ST,Baad_az_Taleghani,kooche_	Hosseini,Pelak293'),
+(0213,'BIX','MR.AHMADFAR',32,02188990053,'Tehran,Gandi,Gandi_Shopping_center,floor2,pelak39'),
+(0214,'BOOF','MR.Rezafar',32,021966087863,'Tehran,Valiasr,Zaaferanie'),
+(0215,'PARIS','MR.Shojaee',32,02193425617,'Tehran,SaadatAbad,AllamehMajlesi,16th_street'),
+(0216,'GILAR','MR.Razeghi',32,02112365432,'Tehran,Niavaran,Tajrish,4_rahe_mojde'),
+(0217,'FARSI','MR.REZAIAN',32,02110998762,'Tehran,Shariati_ST,Dolat_ST,janbe_masjede_gholhack,pelak41'),
+(0218,'TAHCHINEASL','MR.Khaghani',50,02198767852,'Tehran,Resalat,Nabshe_Donyaenoor_Shoppingcenter,pelak870'),
+(0219,'AGAR','MS.REZAEE',40,02123245872,'Tehran,SaadatAbad,SarveGharbi,Golestan_ST,Pelak18'),
+(02110,'ALIGHAPU','MS.Farshadi',45,0218866436,'Tehran,Vanak,Gandi'),
+(02111,'LEON','MR.Sohrabi',40,02188006552,'Tehran,Tajrish,Niavaran,Pelak460'),
+(02112,'LEON2','MR.Tarighat',50,02196907752,'Tehran,ValiAsr,Fereshte_ST,SAM_shopping_center'),
+(02113,'MORVARID','MR.RAHEBI',40,02188684532,'Tehran,Marzdaran,Between_Ashrafi_YadegareEmam'),
+(02114,'BISAN','MR.Rahmani',35,02112874052,'Tehran,Fereshte_ST,Shabdiz_ST,Bonbast_Afshar,Pelak13'),
+(02115,'KANABA','MR.Ziaee',43,02189124152,'Tehran,Jordan,Zafar,Ghable_ValiAsr'),
+(02116,'DIVAN','MS.Ziaee',40,02188609902,'Tehran,Fereshte_ST,Fayazi_ST,SAM_shopping_center,floor8'),
+(02117,'ROOKASHE','MR.Nasr',30,02182317752,'Tehran,Pasdaran,Golestan8,Pelak893'),
+(02118,'HANI','MR.Emami',25,02191827655,'Tehran,ValiAsr,Motahari_ST,1991_Department'),
+(02119,'YAS','MR.Yavari',45,02198991232,'Tehran,ValiAsr,Nabshe_MellatPark'),
+(02120,'TAMASHA','MR.Mehranfar',55,02182313246,'Tehran,ShahrakeGharb,MilladNoor,floor6'),
+(0311,'SARA','MR.RUZBEH',20,03138651252,'Isfahan,Enghelab'),
+(0312,'HERMES','MR.SABERI',32,03138659811,'Isfahan,Nazar,Kooche_Kelisa'),
+(0313,'AAZAM','MR.AGHAEE',22,03134544252,'Isfahan,KamalEsmail'),
+(0314,'SANI','MR.RUZBEHIAN',35,03138609912,'Isfahan,Park_Saadi,Pelak28'),
+(0315,'PIDE','MR.KARAMI',24,03132316782,'Isfahan,Mir,4_Rahe_Mosalla'),
+(0316,'KENTAKI_HOUSE1','MR.KAZEMIAN',30,03138912152,'Isfahan,Nazar'),
+(0317,'KENTAKI_HOUSE2','MR.KAZEMIAN',30,03133212152,'Isfahan,AhmadAbad'),
+(0318,'SIB','MR.KASHANI',21,03136651287,'Isfahan,Atashgah'),
+(0319,'DADA','MR.ROHANI',27,03132314562,'Isfahan,Parvin'),
+(03110,'GIK','MR.KAZEMI',20,03136644253,'Isfahan,Ostandari_ST')
+
+
+
+INSERT INTO CUSTOMER VALUES
+(1272914887,'Ali Rad',9135615029,GETDATE()),
+(1273451488,'Ali Radman',9136615033,GETDATE()),
+(1279914887,'Zahra Zamani',9135556771,GETDATE()),
+(1277112917,'Saeed Razavi',9125007633,GETDATE()),
+(1274607582,'Kiarash Rezaee',9388005436,GETDATE()),
+(1279112647,'Naser Koohi',9133345660,GETDATE()),
+(1273211212,'Hamta Karimi',9125615542,GETDATE()),
+(1274603217,'Hosein Kazemi',9382283245,GETDATE()),
+(1275449221,'Reza Armani',9124609889,GETDATE()),
+(1272119551,'Ali Shojaee',9364543219,GETDATE()),
+(1272814987,'Ali Heidari',9129987659,GETDATE()),
+(1273235667,'Mahdi Moradi',9125345425,GETDATE())
+
+
+
+INSERT INTO SERVICE_FORCE VALUES
+(1104609876,'Raha Yavari','female',913427860,'Isfahan,Parvin','chef',03110,2000000),
+(1104609876,'Ali REZAEE','male',912257860,'Tehran,SattarKhan','manager',0211,5000000),
+(1104788712,'Arman GHAFURI','male',9126778930,'Tehran,Vanak','manager',0212,4000000),
+(1104606679,'Reza Ahmadfar','male',9122284360,'Tehran,TehranPars','manager',0213,3500000),
+(1105556771,'Arya Razeghi','male',912427860,'Tehran,Shariati','manager',216,4500000),
+(1273307661,'Kaveh REZAIAN','male',9123344972,'Tehran,Haghani','manager',217,5000000),
+(1272915766,'Hamed Khaghani','male',9126658700,'Tehran,Enghelab','manager',218,4800000),
+(1278890076,'Farnaz Farshadi','female',938433200,'Tehran,7Tir','manager',2110,4200000),
+(1274605521,'Kasra Sohrabi','male',9362209060,'Tehran,Tehran_Vila','manager',2111,4500000),
+(1273205506,'Kamal Shojaee','male',9121234565,'Tehran,Velenjak','manager',215,5600000),
+(1279878749,'Ali Rezafar','male',9125663890,'Tehran,Niavaran','manager',214,6600000),
+(1273311010,'Kimia REZAEE','male',9122567640,'Tehran,DehVanak','manager',219,5000000),
+(1277761090,'Sobhan Tarighat','male',9123239870,'Tehran,Mirdamad','manager',2112,6500000),
+(1279001022,'Hadi RAHEBI','male',9122706890,'Tehran,Pasdaran','manager',2113,5700000),
+(1278407220,'Pouya Rahmani','male',912259881,'Tehran,Marzdaran','manager',2114,6500000),
+(1273277506,'Kamran Ziaee','male',9126771300,'Tehran,Behesht','manager',2115,5400000),
+(1270090044,'Hellen Ziaee','female',912257860,'Tehran,Piruzi','manager',2116,5300000),
+(1271010321,'Radman Nasr','male',9123567710,'Tehran,Shahed','manager',2117,6600000),
+(1279010320,'Hosein Emami','male',9124808821,'Tehran,Hafez','manager',2118,5900000),
+(1274603290,'Hooman Yavari','male',9124553210,'Tehran,Mirdamad','manager',2119,6400000),
+(1278812011,'Omid Mehranfar','male',9126563100,'Tehran,ValiAsr','manager',2120,5300000),
+(1273111076,'Naser Razavian','male',9125510099,'Tehran,Azadi','chef',2120,4000000),
+(1271233211,'Raana Aghaei','female',9129907711,'Tehran,7Tir','chef',2120,3700000),
+(1271101559,'Sadegh Azadi','male',9122468006,'Tehran,Enghelab','chef',2119,4200000),
+(1271201660,'Karim Rasekhi','male',9121357011,'Tehran,DehVanak','chef',2118,4000000),
+(1271301770,'Ali Sadeghi','male',9123211230,'Tehran,Mofateh','chef',2118,3900000),
+(1271401675,'Zahra Akbari','female',9121203238,'Tehran,7Tir','chef',2119,3800000)
+
+
+
+
+INSERT INTO INVENTORY VALUES
+(0211,CONVERT(DATE,GETDATE()),20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,0),
+(0212,CONVERT(DATE,GETDATE()),30,50,30,25,23,20,40,20,50,20,25,32,24,20,20,15,5,10,15),
+(0213,CONVERT(DATE,GETDATE()),20,20,40,26,20,27,20,30,17,26,43,21,19,20,20,34,22,20,16),
+(0214,CONVERT(DATE,GETDATE()),30,40,40,40,30,40,25,33,41,27,21,20,25,30,15,20,32,22,17),
+(0215,CONVERT(DATE,GETDATE()),30,40,35,28,21,28,30,32,31,28,29,17,32,24,20,20,23,28,18),
+(0216,CONVERT(DATE,GETDATE()),40,40,40,40,40,30,35,25,31,24,25,30,20,30,21,24,32,15,20),
+(0211,CONVERT(DATE,GETDATE()),20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,32,3),
+(2119,CONVERT(DATE,GETDATE()),11,11,23,22,11,12,23,43,43,21,32,22,23,31,14,15,34,40,32)
+
+
+
+INSERT INTO FIXED_CUSTOMER VALUES
+(11,110000,'GREAT'),
+(12,50000,'MODERATE')
+
+
+
+INSERT INTO FOOD_REPORT VALUES
+(1,20,200,1200),
+(2,30,460,1000),	
+(3,45,320,1700),
+(4,32,543,1800),
+(5,40,400,2100),
+(6,28,320,988),
+(7,37,260,1204),
+(8,32,546,1900),
+(9,50,328,1950),
+(10,43,288,1020),
+(11,32,440,2020),
+(12,43,560,1090),
+(13,26,210,1670),
+(14,34,560,2100),
+(15,44,600,1080),
+(16,30,300,1200),
+(17,29,290,1100),
+(18,32,450,1870),
+(19,35,410,2000)
+
+
+
+INSERT INTO COMMENT VALUES
+(12,17,1,'EXELLENT',GETDATE()),
+(11,18,1,'BRING FASTER PLEASE',GETDATE()),
+(7,10,0,'AWFULLLLL',GETDATE()),
+(5,15,1,'GOOD BUT IT WAS SPICY ALOT',GETDATE()),
+(12,15,0,'AWFUL',GETDATE())
